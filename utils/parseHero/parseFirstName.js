1d9944c14c38c2d8febcdabd607c014484421a52
Matthew Cromer <matthew.h.cromer@gmail.com>
2022-05-15 13:02:47 -0700

    fix first name parsing, improve getHero unit tests

--- utils/parseHero/parseFirstName.js
+++ utils/parseHero/parseFirstName.js
@@ -4,9 +4,9 @@ import femaleFirstNames from '../../constants/femaleFirstNames.js';
 export default ({ gender, id }) => {
   let firstName = id;
 
-  if (gender === 'male') firstName = maleFirstNames[id] ?? id;
+  if (gender === '1') firstName = maleFirstNames[id] ?? id;
 
-  if (gender === 'female') firstName = femaleFirstNames[id] ?? id;
+  if (gender === '3') firstName = femaleFirstNames[id] ?? id;
 
   if (firstName !== id) return firstName[0].toUpperCase() + firstName.slice(1);
   else return firstName;
