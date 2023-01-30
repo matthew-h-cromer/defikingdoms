a2a90859a375d78a94ca85b07351ab5961494e01
Matthew Cromer <matthew.h.cromer@gmail.com>
2023-01-29 21:02:30 -0800

    update contract addresses for crystalvale

--- constants/contractAddresses.js
+++ constants/contractAddresses.js
@@ -1,5 +1,10 @@
 export default {
-  hero: '0x5f753dcdf9b1ad9aabc1346614d1f4746fd6ce5c',
-  salesAuction: '0x13a65B9F8039E2c032Bc022171Dc05B30c3f2892',
-  perilousJourney: '0xE92Db3bb6E4B21a8b9123e7FdAdD887133C64bb7',
+  crystalvale: {
+    hero: '0xEb9B61B145D6489Be575D3603F4a704810e143dF',
+    salesAuction: '0xc390faa4c7f66e4d62e59c231d5bed32ff77bef0',
+  },
+  serendale: {
+    hero: '',
+    salesAuction: '',
+  },
 };
