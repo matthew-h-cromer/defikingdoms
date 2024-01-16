45941b570a5484daeced53975940bcff29941421
Matthew Cromer <matthew.h.cromer@gmail.com>
2024-01-16 15:49:03 -0800

    add crafting constants

---
+++ constants/crafting.js
@@ -0,0 +1,10 @@
+export default {
+  0: 'blacksmithing',
+  2: 'goldsmithing',
+  4: 'armorsmithing',
+  6: 'woodworking',
+  8: 'leatherworking',
+  10: 'tailoring',
+  12: 'enchanting',
+  14: 'alchemy'
+};
