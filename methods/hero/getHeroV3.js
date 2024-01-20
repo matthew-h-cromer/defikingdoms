246803404370bae92568cab23d742904f30a3bde
Matthew Cromer <matthew.h.cromer@gmail.com>
2024-01-19 21:35:05 -0800

    accept retries param for getHeroV3 and getHeroesV3

--- methods/hero/getHeroV3.js
+++ methods/hero/getHeroV3.js
@@ -2,10 +2,12 @@ import retry from '../../utils/retry.js';
 import parseHeroV3 from '../../utils/parseHeroV3/parseHeroV3.js';
 
 export default async function (tokenId, options) {
-  const rawHero = await retry(() =>
-    this.heroContract.methods
-      .getHeroV3(tokenId)
-      .call(null, options?.blockNumber)
+  const rawHero = await retry(
+    () =>
+      this.heroContract.methods
+        .getHeroV3(tokenId)
+        .call(null, options?.blockNumber),
+    options?.retries
   );
 
   const parsedHero = parseHeroV3(rawHero);
